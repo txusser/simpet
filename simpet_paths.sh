feb40bee8e0cbbe6770e7ae36f97e2a9d4984132
txusser <jesus.bubuchis@gmail.com>
2022-12-29 12:12:08 +0100

    Modified the model for the Inveon

---
+++ simpet_paths.sh
@@ -0,0 +1,3 @@
+export PATH=/home/jesus/simpet/include/fruitcake/bin:$PATH
+export LD_LIBRARY_PATH=/home/jesus/simpet/include/fruitcake/book/lib:$LD_LIBRARY_PATH
+export PATH=/home/jesus/simpet/include/format_converters:$PATH

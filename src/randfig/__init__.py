d58bab0b984c33ebea4fc9bab924a385be8da694
YerePhy <jereasap@gmail.com>
2023-03-15 13:51:28 +0100

    Adding new classes to __init__.py.

--- src/randfig/__init__.py
+++ src/randfig/__init__.py
@@ -2,6 +2,10 @@ from .compose import *
 from .config_transform import *
 from .formula import *
 from .nest import *
+from .unnest import *
+from .save import *
+from .insert import *
+from .remove import *
 
 
 __all__ = (
@@ -9,4 +13,8 @@ __all__ = (
     + config_transform.__all__
     + formula.__all__
     + nest.__all__
+    + unnest.__all__
+    + save.__all__
+    + insert.__all__
+    + remove.__all__
 )

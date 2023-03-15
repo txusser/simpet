1d03920f9881ab5a996d623fd4e877629e96fb72
YerePhy <jereasap@gmail.com>
2023-03-15 13:52:24 +0100

    Adding a nested dummy config to fixtures files (conftest.py).

--- src/randfig/tests/conftest.py
+++ src/randfig/tests/conftest.py
@@ -5,3 +5,10 @@ import pytest
 def config():
     return {"param_0": 0, "param_1": 1, "param_2": 2}
 
+
+@pytest.fixture(scope="module")
+def nested_config():
+    return {
+        "param_root":
+            {"param_nest":
+                {"param_0": 0, "param_1": 1}}}

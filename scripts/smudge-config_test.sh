6937f06f3b5983a5c2540b732931bb3244de4d76
YerePhy <jereasap@gmail.com>
2023-04-19 18:18:33 +0200

    Trying to fix configs.

--- scripts/smudge-config_test.sh
+++ scripts/smudge-config_test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 export GIT_DIR='${root_path:root}'
-export INCLUDE_DIR="${GIT_DIR}"/include
-export SIMSET_DIR="${INCLUDE_DIR}"/SimSET/2.9.2
-export STIR_DIR="${INCLUDE_DIR}"/STIR/install
+export INCLUDE_DIR="$GIT_DIR"/include
+export SIMSET_DIR="$INCLUDE_DIR"/SimSET/2.9.2
+export STIR_DIR="$INCLUDE_DIR"/STIR/install
 
-yq eval \
+yq -r eval \
     '.dir_stir = strenv(STIR_DIR) |
     .dir_simset = strenv(SIMSET_DIR)' -
 

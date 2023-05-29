a17c305c8652d6d35f095d34023715c741cbcd38
YerePhy <jereasap@gmail.com>
2023-05-29 16:02:56 +0200

    Updating filters.

---
+++ scripts/clean-config_test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+export DIR_STIR='${root_path:root}/include/STIR/install'
+export DIR_SIMSET='${root_path:root}/include/SimSET/2.9.2'
+
+yq eval \
+    '.defaults[0].params = "test" |
+    .interactive_mode = 0 | 
+    .dir_stir = strenv(DIR_STIR) | 
+    .dir_simset = strenv(DIR_SIMSET) | 
+    .matlab_mcr_path = "" | 
+    .spm_path = "" | 
+    .dir_data_path = null | 
+    .dir_results_path = null | 
+    .interactive_mode = 0 | 
+    .stratification = "true" | 
+    .forced_detection = "true" | 
+    .forced_non_absortion = "true" | 
+    .acceptance_angle = 90.0 | 
+    .positron_range =  "true" | 
+    .isotope = "f18" | 
+    .non_colinearity = "true" | 
+    .minimum_energy = 350.0 | 
+    .weight_window_ratio = 1.0 | 
+    .point_source_voxels = "false" | 
+    .coherent_scatter_object = "false" | 
+    .coherent_scatter_detector = "false"' -
+

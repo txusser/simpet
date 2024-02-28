d685bbdf39075696a29465b3bbd83e20c53efb53
Yere Poveda <jereasap@gmail.com>
2024-02-28 13:09:49 +0100

    Reading vars from .env file and using -bash to get access to simpet repo in the script.

--- scripts/launch_experiment_set.sh
+++ scripts/launch_experiment_set.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # Variable are defined in the ENVFILE
 
-ENVFILE="$SIMPET"/.env
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+SIMPET_DIR="$(dirname "$SCRIPT_DIR")"
+LOGS_DIR="$SIMPET_DIR"/logs
+ENVFILE="$SIMPET_DIR"/.env
 
-mkdir -p "$SIMPET"/logs
+mkdir -p "$LOGS_DIR"
 source "$ENVFILE"
 nohup $VENV -u "$SIMPET"/scripts/experiment_set.py >"$SIMPET"/logs/simulation_set.log 2>&1 &

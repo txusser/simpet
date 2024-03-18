35c7b04fbb8f569e511a3eb4058c688d3bb6e440
Yere Poveda <jereasap@gmail.com>
2024-03-18 17:10:29 +0100

    Adding a launcher for whole body simulations.

---
+++ scripts/experiment_wholebody.py
@@ -0,0 +1,29 @@
+import sys
+import hydra
+import yaml
+from typing import Union, Mapping, Any
+from pathlib import Path
+from pyprojroot import here
+from omegaconf import DictConfig, OmegaConf
+
+sys.path.append(str(here()))
+import wholebody
+
+
+try:
+    OmegaConf.register_new_resolver("root_path", lambda s: str(here()))
+    OmegaConf.register_new_resolver("home_path", lambda s: str(Path.home()))
+except ValueError:
+    pass
+
+
+@hydra.main(version_base=None, config_path=str(here().joinpath("configs")), config_name="config_test_wholebody")
+def simulate(cfg: DictConfig) -> None:
+    OmegaConf.resolve(cfg)
+    wb = wholebody.WholebodySimulation(cfg)
+    wb.run()
+    cfg = OmegaConf.to_container(cfg)
+
+
+if __name__ == "__main__":
+    simulate()

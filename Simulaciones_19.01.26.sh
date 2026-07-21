98e74756cfd18274902975bad16dafcc16004231
claudiadom <163831034+claudiadom@users.noreply.github.com>
2026-07-21 14:31:28 +0200

    Delete Simulaciones_19.01.26.sh

--- Simulaciones_19.01.26.sh
+++
@@ -1,8 +0,0 @@
-# Simulaciones  NEC
-#!/bin/bash
-#python scripts/experiment_wholebody_claudia.py --config-name config_test_wholebody_reference_uniformidad_1
-#python scripts/experiment_wholebody_claudia.py --config-name config_test_wholebody_reference_uniformidad_2
-#python scripts/experiment_wholebody_claudia.py --config-name config_test_WB_reference_agua_plast
-#python scripts/experiment_wholebody_claudia.py --config-name config_test_wholebody_reference_uniformidad_3
-python scripts/experiment_wholebody_claudia.py --config-name config_test_WB_Phantom_Rat_10.11.25_plast_26
-python scripts/experiment_wholebody_claudia.py --config-name config_test_WB_Phantom_Rat_10.11.25_plast_28

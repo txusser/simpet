885374f7a947e5499b71ab8c3acf734b86c26c63
claudiadom <163831034+claudiadom@users.noreply.github.com>
2026-07-21 14:32:22 +0200

    Delete Simulaciones_23.01.26.sh

--- Simulaciones_23.01.26.sh
+++
@@ -1,8 +0,0 @@
-# Simulaciones  NEC
-#!/bin/bash
-
-python scripts/experiment_wholebody_claudia.py --config-name config_test_wholebody_reference_uniformidad_1
-python scripts/experiment_wholebody_claudia.py --config-name config_test_wholebody_reference_uniformidad_2
-python scripts/experiment_wholebody_claudia.py --config-name config_test_WB_reference_todo_igual_1
-python scripts/experiment_wholebody_claudia.py --config-name config_test_WB_reference_todo_igual_3
-
